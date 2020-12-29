e3e8774fc912ee19a9139b2ddaecb5a4247694f2
henriquevital00 <henriquevital00@gmail.com>
2020-12-28 21:12:05 -0300

    added preprocessing of udemy course

---
+++ preprocessing/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Import dataset
+dataset = pd.read_csv('./Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding independent variable
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])],
+                       remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Encoding dependend variable
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+
+# Split dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X,
+                                                    Y,
+                                                    test_size=0.2,
+                                                    random_state=1)
+
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(x{:, 3:})
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+
